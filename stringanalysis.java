2b5ccc2081a85f23261abca595ebe008f0742458
star-0111 <beboosos50@gmail.com>
2025-07-08 21:36:24 +0300

    stringanalysis.java

--- stringanalysis.java
+++ stringanalysis.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class assignment2 {
+public class stringanalysis {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
