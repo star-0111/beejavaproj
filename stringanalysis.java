4b52b3601579889b1a7df5ee383a6994c45a7271
star-0111 <beboosos50@gmail.com>
2025-07-08 21:40:07 +0300

    Update stringanalysis.java

--- stringanalysis.java
+++ stringanalysis.java
@@ -4,20 +4,19 @@ public class stringanalysis {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Ask user for a sentence
         System.out.print("Enter a sentence: ");
         String sentence = input.nextLine();
 
-        // Convert to uppercase
+
         String uppercaseSentence = sentence.toUpperCase();
 
-        // Get length
+      
         int length = sentence.length();
 
-        // Get first character
+
         char firstChar = sentence.charAt(0);
 
-        // Display results
+
         System.out.println("\nUppercase: " + uppercaseSentence);
         System.out.println("Length: " + length);
         System.out.println("First character: " + firstChar);
