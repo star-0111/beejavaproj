41ca3d485a9435b57cd1c414fdc44a08fbed75cd
star-0111 <beboosos50@gmail.com>
2025-07-08 21:31:07 +0300

    Create assignment2.java

---
+++ assignment2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class assignment2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask user for a sentence
+        System.out.print("Enter a sentence: ");
+        String sentence = input.nextLine();
+
+        // Convert to uppercase
+        String uppercaseSentence = sentence.toUpperCase();
+
+        // Get length
+        int length = sentence.length();
+
+        // Get first character
+        char firstChar = sentence.charAt(0);
+
+        // Display results
+        System.out.println("\nUppercase: " + uppercaseSentence);
+        System.out.println("Length: " + length);
+        System.out.println("First character: " + firstChar);
+
+        input.close();
+    }
+}
